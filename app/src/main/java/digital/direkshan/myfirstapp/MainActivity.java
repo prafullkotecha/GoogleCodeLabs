9b81313df1424e579e91e71df9734d70c50e1e5a
Kotecha, Prafulla <prafulla.kotecha@ingramcontent.com>
2017-09-26 14:04:02 -0400

    Updated with code for branch challenge-deep linking- at droidconNYC 2017

--- app/src/main/java/digital/direkshan/myfirstapp/MainActivity.java
+++ app/src/main/java/digital/direkshan/myfirstapp/MainActivity.java
@@ -3,9 +3,15 @@ package digital.direkshan.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TOTAL_COUNT = "total_count";
@@ -63,5 +69,29 @@ public class MainActivity extends AppCompatActivity {
         // Start the new activity.
         startActivity(randomIntent);
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Branch branch = Branch.getInstance();
+
+        branch.initSession(new Branch.BranchReferralInitListener(){
+            @Override
+            public void onInitFinished(JSONObject referringParams, BranchError error) {
+                if (error == null) {
+                    // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
+                    // params will be empty if no data found
+                    // ... insert custom logic here ...
+                } else {
+                    Log.i("MyFirstApp", error.getMessage());
+                }
+            }
+        }, this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        this.setIntent(intent);
     }
 }
